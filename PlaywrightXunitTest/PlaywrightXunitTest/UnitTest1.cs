bce81ac3d4936ea07e846fb850f247561bf16a16
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2024-11-30 00:24:22 +1000

    fix test example

--- PlaywrightXunitTest/PlaywrightXunitTest/UnitTest1.cs
+++ PlaywrightXunitTest/PlaywrightXunitTest/UnitTest1.cs
@@ -9,15 +9,15 @@ namespace PlaywrightXunitTest
         {
             // Initialize Playwright
             using var playwright = await Playwright.CreateAsync();
-            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
             var page = await browser.NewPageAsync();
 
             // Navigate to a website
-            await page.GotoAsync("https://example.com");
+            await page.GotoAsync("https://google.com");
 
             // Perform actions and assertions
             var title = await page.TitleAsync();
-            Assert.Equal("Example Domain", title);
+            Assert.Equal("Google", title);
 
             // Close the browser
             await browser.CloseAsync();
