bd71df5fdd732813d2a062838b7b90ab4249f2e1
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2024-12-06 08:18:47 +1000

    Add the test that using playwright fixture

---
+++ PlaywrightXunitTest/PlaywrightXunitTest/Example/TestingUsingPlaywrigthFixture.cs
@@ -0,0 +1,53 @@
+﻿using PlaywrightXunitTest.Fixtures;
+
+namespace PlaywrightXunitTest.Example
+{
+    //Use Collection attribute to apply the PlaywrightFixture to the test class
+    //Inherit the IAsyncLifetime to implement the InitializeAsync and DisposeAsync methods
+    [Collection("Playwright collection")]
+    public class TestingUsingPlaywrigthFixture : IAsyncLifetime
+    {
+        //create field for the PlaywrightFixture and IPage
+        private readonly PlaywrightFixture _playwrightFixture;
+        private IPage _page;
+
+        //inject the PlaywrightFixture to the constructor (this will automaticly from collection)
+        public TestingUsingPlaywrigthFixture(PlaywrightFixture playwrightFixture)
+        {
+            _playwrightFixture = playwrightFixture;
+            _page = null!;//initialize the page to avoid warning
+        }
+        //implement the DisposeAsync method from IAsyncLifetime
+        public async Task DisposeAsync()
+        {
+            if (_page != null)
+                await _page.CloseAsync();
+        }
+        //implement the InitializeAsync method from IAsyncLifetime
+        public async Task InitializeAsync()
+        {
+            _page = await _playwrightFixture.BrowserContext.NewPageAsync();
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            await _page.GotoAsync("https://google.com");
+            
+            var title = await _page.TitleAsync();
+
+            Assert.Equal("Google", title);
+        }
+
+        [Fact]
+        public async Task ApplyCheckTitle()
+        {
+            
+            await _page.GotoAsync("https://www.2apply.com.au/login");
+           
+            var title = await _page.TitleAsync();
+
+            Assert.Equal("2Apply Free Fast Rental Application Form", title);
+        }
+    }
+}
--- PlaywrightXunitTest/PlaywrightXunitTest/Fixtures/PlaywrightFixture.cs
+++ PlaywrightXunitTest/PlaywrightXunitTest/Fixtures/PlaywrightFixture.cs
@@ -49,5 +49,14 @@ namespace PlaywrightXunitTest.Fixtures
 
             return builder.Build();
         }
+
+
+    }
+    [CollectionDefinition("Playwright collection")]
+    public class PlaywrightCollection : ICollectionFixture<PlaywrightFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
     }
 }
